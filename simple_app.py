96db491817a3ceb8ccfacfb6fbfdba9a19c13c5a
SphagnumShuffle <ville_makinen@outlook.com>
2020-02-07 16:22:53 +0200

    added Full Name to make plot more informative

--- simple_app.py
+++ simple_app.py
@@ -13,6 +13,7 @@ from certain clubs and nationalities
 
 df = st.cache(pd.read_csv)('nhl_players.csv')
 
+
 teams = st.sidebar.multiselect('Show Player for clubs? (abbreviation)', df['Team'].unique())
 
 nationalities = st.sidebar.multiselect('Show Players from Nationalities?',
@@ -21,24 +22,26 @@ df['Cntry'].unique())
 # Filter dataframe 
 new_df = df[(df['Team'].isin(teams)) &
 (df['Cntry'].isin(nationalities))]
+new_df['Full Name'] = df['First Name'] + ' ' + df['Last Name']
+
 
 # Write dataframe to screen
-cols = ['Last Name', 'First Name', 'Cntry', 'Position', 'GP', 'G', 'A', 'PTS', 'Salary','Cap Hit']
+cols = ['Last Name', 'First Name', 'Full Name', 'Cntry', 'Position', 'GP', 'G', 'A', 'PTS', 'Salary','Cap Hit']
 if st.checkbox('Show dataframe', value=True):
     st.write(new_df[cols])
 # st.write(new_df[cols])
 
 # Create figure using plotly express
 # options = st.sidebar.multiselect('Select columns', new_df.columns)
-x = st.sidebar.selectbox('Choose variable in X-axis', cols, index=9)
+x = st.sidebar.selectbox('Choose variable in X-axis', cols, index=10)
 # cols = [i for i in cols if i != x]
 # x = options[0]
-y = st.sidebar.selectbox('Choose variable in Y-axis', cols, index=7)
+y = st.sidebar.selectbox('Choose variable in Y-axis', cols, index=8)
 # y = options[1]
-color = st.sidebar.selectbox('Choose variable in Color-axis', cols, index=2)
+color = st.sidebar.selectbox('Choose variable in Color-axis', cols, index=3)
 # color = options[2]
 if teams and nationalities and x and y and color:
-    fig = px.scatter(new_df, x=x, y=y, color=color, hover_name='Last Name')
+    fig = px.scatter(new_df, x=x, y=y, color=color, hover_name='Full Name')
 
     # Plot it
     st.plotly_chart(fig)
